b078d3afc2d93f337bf8e4f4408b6f647e3accca
Rene <rene.soto01@iest.edu.mx>
2025-10-31 18:07:37 -0600

    Configuraciones iniciales del proyecto en equipo con Valeria

---
+++ Tabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tabs
+//
+//  Created by MAESTRO603 on 31/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tabs/TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsApp.swift
+//  Tabs
+//
+//  Created by MAESTRO603 on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
